2a40de86539723c2b2d7d0ca6b1e1bab98ac5f67
Gabriela <gabriela.rodrigues@chatguru.com.br>
2022-04-12 19:41:29 -0300

    css pronto

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="./images/favicon-32x32.png"
+  />
+
+  <title>Frontend Mentor | Advice generator app</title>
+
+  <div class="tudo">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './css/global.scss'
+/* import axios from 'axios' */
+
+createApp(App).mount('#app')
